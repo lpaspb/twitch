99d3185f62743c2f64c3c8284ebef91402601c37
lpaspb <lpaspb97@gmail.com>
2017-02-06 14:13:50 +0300

    safari

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,27 @@ var apiKey = '3d6ea7e572d48a4286db05fd9da78c91';
 var apiSource = 'https://cors.now.sh/http://api.openweathermap.org/data/2.5/weather?';
 
 
+function formattedDate (){
+     var monthNames = [
+                        "January", "February", "March",
+                        "April", "May", "June", "July",
+                        "August", "September", "October",
+                        "November", "December"
+                            ];
+
+        var date = new Date();
+        var day = date.getDate();
+        var monthIndex = date.getMonth();
+        var year = date.getFullYear();
+
+        var date = day + ' ' + monthNames[monthIndex] + ' ' + year;
+    
+        return date;
+}
+
+
+
+
 $( document ).ready(function() {
     
     
@@ -38,21 +59,9 @@ $( document ).ready(function() {
                 city = data.name + ',' + ' ' + data.sys.country;
             }
             
-            
-        var monthNames = [
-                        "January", "February", "March",
-                        "April", "May", "June", "July",
-                        "August", "September", "October",
-                        "November", "December"
-                            ];
-
-        var date = new Date();
-        var day = date.getDate();
-        var monthIndex = date.getMonth();
-        var year = date.getFullYear();
-
-        var date = day + ' ' + monthNames[monthIndex] + ' ' + year;
-        var url = "http://openweathermap.org/img/w/" + icon;
+        var date = formattedDate(); 
+       
+        var url = "https://cors.now.sh/http://openweathermap.org/img/w/" + icon;
         var png = ".png"
         $('.weather__city').html(city);
         $('.weather__date').html(date);
