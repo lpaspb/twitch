8e860173ebe0ba116c05667f5aa163703cfe5a57
lpaspb <lpaspb97@gmail.com>
2017-02-08 20:00:05 +0300

    clear code

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,21 @@
-
+var corsURL = "https://cors.now.sh/";
 
 window.onload = function() {
 
-var btn = document.querySelector("#btn");
-var btn2 = document.querySelector("#btn2");
+    var btn = document.querySelector("#btn");
+    var btn2 = document.querySelector("#btn2");
 
- loadRandom();
+    loadRandom();
 
-btn.addEventListener('click', function(){
-  
-    loadAjax("//en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srprop=snippet&srsearch=");
-    
+    btn.addEventListener('click', function() {
+
+        loadAjax("//en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=search&srprop=snippet&srsearch=");
+
     }, false);
-    
-btn2.addEventListener('click', function(){
-  
-     loadRandom();
+
+    btn2.addEventListener('click', function() {
+
+        loadRandom();
 
     }, false);
 
@@ -25,165 +25,127 @@ btn2.addEventListener('click', function(){
 
 function resetContent() {
 
-var wiki = document.querySelector("#wiki");
+    var wiki = document.querySelector("#wiki");
 
-wiki.innerHTML = '';
-
+    wiki.innerHTML = '';
 }
 
+function addElementInBlock (elementInner, eltag, blockID) {
+                        
+    var element = document.createElement(eltag);
+    element.innerHTML = elementInner; 
+    var block = document.getElementById(blockID);
+    block.appendChild(element);
+ }
 
-
-
-function loadAjax (url) {
-
-resetContent();
-
-
-    
-var inputValue = document.querySelector("input[type=search]").value;
-var url;
-var xhr = new XMLHttpRequest;
-var title = inputValue; 
-
-var ua = navigator.userAgent.toLowerCase(); 
-if (ua.indexOf('safari') != -1) { 
-  if (ua.indexOf('chrome') > -1) {
-   url = url + title;
-  } else {
-  url = "https://cors.now.sh/" + url + title;
-  }
-}
-    
-xhr.open('GET', url, true);
-
-xhr.onreadystatechange = function() {
-
-    var response = xhr.responseText;
-    if (response) {
-    var json = JSON.parse(response);
-    }
-    
-    
-    if (response != "" && xhr.readyState == 4) {
-     
-     var inputValue = document.querySelector("input[type=search]").value;
-      
-     if (inputValue != "")   {     
+function formattedURL (url) {                           
         
-        var feedArr = json.query.search;
+        var inputValue = document.querySelector("input[type=search]").value;
+        var queryTitle = inputValue;
         
-     }
+    
+        var formattedURL = url;
+        var userAgent = navigator.userAgent.toLowerCase();
         
-        console.log(inputValue)
-         
-        if (inputValue != "")   {  
-          
-             for (var i = 0; i < feedArr.length; i++){
-           
-                
-               var title = "<p><a href='http://en.wikipedia.org/wiki/" + 
-                   
-                json.query.search[i].title + "'> <strong id='title'>" + 
-                   
-                json.query.search[i].title +  "</strong></a></p>";
-                
-                
-                var desc = "<p id='desc'>" + json.query.search[i].snippet +  "</p>";
-
-                var element = document.createElement("div");
-
-                element.innerHTML = title + "\n" + desc;
-
-
-                my_div = document.getElementById("wiki");
-
-                my_div.appendChild(element);
-
-              
-           }
+        if (userAgent.indexOf('safari') != -1 && formattedURL.indexOf("random") == -1) {
+            if (userAgent.indexOf('chrome') > -1) {
+                 formattedURL = formattedURL + queryTitle;
+                 return formattedURL;
+            } else {
+                formattedURL = corsURL + formattedURL + queryTitle;
+                return formattedURL;
+            }
+        } else {
+            if (userAgent.indexOf('chrome') > -1) {
+                 formattedURL = formattedURL;
+                 return formattedURL;
+            } else {
+                formattedURL = corsURL + formattedURL;
+                return formattedURL;
+            }
+            
         }
-        
-        
-            else   {
-    
-               var element = document.createElement("p");
-
-               element.innerHTML = "No Articles";
-
-               my_div = document.getElementById("wiki");
-
-               my_div.appendChild(element);
-
-        
-    
-}
-          
-       
-        
-    } 
-        
-    
-  
-}
+    }
 
 
+function loadAjax(url) {
 
-xhr.send();
-
-
-}
-
-
-
-
-function loadRandom () {
-   
     resetContent();
 
-    var url;
-    var ua = navigator.userAgent.toLowerCase(); 
-
-    if (ua.indexOf('safari') != -1) { 
-  if (ua.indexOf('chrome') > -1) {
-   
-  url = "//en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=5";;
-  } else {
-  url = "https://cors.now.sh/" + "//en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=5";;
-  }
-}
+    var inputValue = document.querySelector("input[type=search]").value;
     
+    url = formattedURL(url);
     
     var xhr = new XMLHttpRequest;
     
-    xhr.open('GET', url);
-   
+    xhr.open('GET', url, true);
+
     xhr.onreadystatechange = function() {
+
+        var response = xhr.responseText;
         
-           var response = xhr.responseText;
-            
-            if (response) {
+        if (response != "" && xhr.readyState == 4) {
             
             var json = JSON.parse(response);
-            
-            console.log(json);
-               
-            var randArray = json.query.random;
-            
-            var title  = "http://en.wikipedia.org/wiki/" + 
-                   
-             json.query.random[0].title;
+
+            if (inputValue != "") {
+
+                var feedArr = json.query.search;
+
+                for (var i = 0; i < feedArr.length; i++) {
+                    
+                    var title = "<p><a href='http://en.wikipedia.org/wiki/" +
+
+                        json.query.search[i].title + "'> <strong id='title'>" +
+
+                        json.query.search[i].title + "</strong></a></p>";
+
+                    var desc = "<p id='desc'>" + json.query.search[i].snippet + "</p>";
+
+                    addElementInBlock (title + "\n" + desc, "p", "wiki");
+
+                }
+            } else {
                 
-             document.querySelector("#btn2").setAttribute("href", title)  ; 
-                
-            
+                addElementInBlock ("No Articles", "p", "wiki");
+
+            }
         }
-            
-            
     }
-        
-    
-    
-    
+
     xhr.send();
+}
+
+
+
+function loadRandom() {
+
+    resetContent();
+
+    var apiURL = "//en.wikipedia.org/w/api.php?origin=*&action=query&format=json&list=random&rnlimit=5";
     
+    formattedURL(apiURL)
+
+    var xhr = new XMLHttpRequest;
+
+    xhr.open('GET', corsURL + apiURL);
+
+    xhr.onreadystatechange = function() {
+
+        var response = xhr.responseText;
+
+        if (response) {
+
+            var json = JSON.parse(response);
+
+            var randArray = json.query.random;
+
+            var title = "http://en.wikipedia.org/wiki/" +
+            json.query.random[0].title;
+
+            document.querySelector("#btn2").setAttribute("href", title);
+        }
+    }
+
+    xhr.send();
 }
